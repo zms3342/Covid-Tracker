5b8c7429430acef349797765ec95d394167ffece
Zafar <zms3342@gmail.com>
2020-07-31 15:27:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import {
+  FormControl, 
+  Select,
+  MenuItem} from '@material-ui/core';
 
 function App() {
+  const [countries, setCountries] =useState([])
+
+  useEffect(()=>{
+    //async to get data 
+    const countriesData = async() =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data) => {
+        //reformat data
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+        //set state countries
+        setCountries(countries);
+       })
+    };
+    //call sub function
+    countriesData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select
+        varient="outline"
+        value="worldwide"
+        onChange="" >
+          {
+            countries.map((country) => (
+              <MenuItem value ={country.value}>{country.name}</MenuItem>
+              )
+            )}
+        </Select>
+      </FormControl>
+      </div>
     </div>
   );
 }
