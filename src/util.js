f7dd5de0ec425f02eaa03c2ba5aa723a601c78e8
Zafar <zms3342@gmail.com>
2020-07-31 22:36:07 -0400

    Fix: util sort functiion

--- src/util.js
+++ src/util.js
@@ -2,14 +2,6 @@
 //sort list by case count
 export const sortData = (data) => {
     const sortedData = [...data];
-    //a= {country, cases}
-    sortedData.sort((a,b) =>{
-        if (a.cases > b.cases) {
-            return -1;
-        }
-        else {
-            return 1;
-        }
-    })
-    return sortedData;
+    
+    return sortedData.sort((a,b) => (a.cases > b.cases ? -1 : 1));
 }
